5fa20c3c19968295eeee1e4e7c9e58b57e710949
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-25 07:43:20 +1100

    more practice problems using object and pointer patterns

--- practiceProblems.js
+++ practiceProblems.js
@@ -86,3 +86,86 @@ const areThereDuplicates = (...inputs) => {
 // console.log(areThereDuplicates('a', 'b', 'c', 'a'));
 // console.log(areThereDuplicates());
 
+const averagePair = (arr, num) => {
+    // check to ensure array and num are both not null values
+
+    // have a start pointer at the start, and an end pointer at the end
+    // if average of them is more than what we need, reduce the end pointer by one
+    // until either the average is found, or we reach a value lower than the desired average in which case we return false if the start pointer has crossed the end pointer, or move on to the next pointer
+
+    let startPointer = 0;
+    let endPointer = arr.length - 1;
+
+    while (startPointer < endPointer) {
+        const desiredAverage = (arr[startPointer] + arr[endPointer]) / 2;
+        if (desiredAverage === num) {
+            return true;
+        }
+        else if (desiredAverage > num) {
+            endPointer--;
+        }
+        else {
+            startPointer++;
+        }
+    }
+    return false;
+};
+
+
+
+
+// console.log(averagePair([1, 2, 3], 2.5)) // true
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)) // true
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)) // false
+// console.log(averagePair([], 4)) // false
+
+
+
+const isSubsequence = (toFind, target) => {
+    let targetArr = target.split("");
+    let startPointer = 0;
+    let result = "";
+    for (const index in toFind) {
+        const resultIndex = targetArr.indexOf(toFind[index], startPointer);
+        if (resultIndex !== -1) {
+            startPointer = resultIndex + 1;
+            if (index <= startPointer) {
+                result += toFind[index];
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    if (result === toFind) {
+        return true;
+    }
+    return false;
+
+
+    // Alternative soln
+    // let i = 0, j = 0;
+    // while (j < str2.length) {
+    //     if (str[i] === str[j]) {
+    //         i++;
+    //     }
+    //     if (i === str1.length) {
+    //         return true;
+    //     }
+    //     j++;
+    // }
+    // return false;
+};
+
+
+
+
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+// 
+console.log(isSubsequence('abc', 'acb')); // false (order matters)
+console.log(isSubsequence('abcdea', 'abceda')); // false (order matters)
